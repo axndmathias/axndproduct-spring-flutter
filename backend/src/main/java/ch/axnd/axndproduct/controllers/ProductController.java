4d11707627fcf3c434c792edfaa40127cc4d4feb
Alexandre Mathias <mathias.alexandre@gmail.com>
2022-04-24 19:08:43 +0200

    class ProductController added

---
+++ backend/src/main/java/ch/axnd/axndproduct/controllers/ProductController.java
@@ -0,0 +1,10 @@
+package ch.axnd.axndproduct.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/products")
+public class ProductController {
+
+}
