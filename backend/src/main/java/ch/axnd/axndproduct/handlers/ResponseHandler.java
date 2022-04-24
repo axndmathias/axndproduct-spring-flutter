47bbeef53610c75b0e927afb7b4c7a6dbc4bfc71
Alexandre Mathias <mathias.alexandre@gmail.com>
2022-04-24 18:24:30 +0200

    class ResponseHandler added

---
+++ backend/src/main/java/ch/axnd/axndproduct/handlers/ResponseHandler.java
@@ -0,0 +1,21 @@
+package ch.axnd.axndproduct.handlers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class ResponseHandler {
+    public static ResponseEntity<Object> handlResponse(String message, HttpStatus status, Object responseObj) {
+        Map<String, Object> responseMap = new HashMap<String, Object>();
+        responseMap.put("message", message);
+        responseMap.put("status", status.value());
+        if (status == HttpStatus.OK) {
+            responseMap.put("data", responseObj);
+        } else {
+            responseMap.put("errors", responseObj);
+        }
+        return new ResponseEntity<>(responseMap, status);
+    }
+}
