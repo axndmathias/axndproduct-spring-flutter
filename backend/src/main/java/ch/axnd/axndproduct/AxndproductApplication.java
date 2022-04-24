22cabce0746c2888e69c704997afad3768c80131
Alexandre Mathias <mathias.alexandre@gmail.com>
2022-04-24 18:03:19 +0200

    backend project created

---
+++ backend/src/main/java/ch/axnd/axndproduct/AxndproductApplication.java
@@ -0,0 +1,13 @@
+package ch.axnd.axndproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxndproductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxndproductApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/ch/axnd/axndproduct/AxndproductApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.axnd.axndproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxndproductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
